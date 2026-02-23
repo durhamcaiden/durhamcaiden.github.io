8dfd421061ba77a0194f59166f79056b0f05c5cd
durhamcaiden <caiden.durham@siu.edu>
2026-02-22 22:39:58 -0600

    Update nav.js

--- js/nav.js
+++ js/nav.js
@@ -1,10 +1,8 @@
 /*
-Name: Caiden Durham
-Course: Lab 3 - Responsiveness & SlickNav
-File: nav.js
+Caiden Durham
 Date Developed: February 2026
 Date Tested: February 2026
-Description: Initialize mobile SlickNav-style menu.
+mobile SlickNav-style menu.
 */
 
 document.addEventListener('DOMContentLoaded', function () {
