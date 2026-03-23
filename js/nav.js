745ea32e588f805e119c75e51c8862ca537091a2
durhamcaiden <caiden.durham@siu.edu>
2026-03-22 23:28:15 -0500

    Update nav.js

--- js/nav.js
+++ js/nav.js
@@ -4,7 +4,6 @@ Date Developed: February 2026
 Date Tested: February 2026
 mobile SlickNav-style menu.
 */
-
 document.addEventListener('DOMContentLoaded', function () {
     buildSlickNav('#menu > ul', {
         prependTo: '#mobile-menu-container',
