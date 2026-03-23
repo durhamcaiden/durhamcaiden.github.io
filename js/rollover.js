ac47a4f2f907b056fd662a89e23a03691ba84ab3
durhamcaiden <caiden.durham@siu.edu>
2026-03-22 23:28:46 -0500

    Update rollover.js

--- js/rollover.js
+++ js/rollover.js
@@ -1,5 +1,5 @@
 function swapMainGraphic(isHover) {
     var img = document.getElementById('mainGraphic');
     if (!img) return;
-    img.src = isHover ? 'images/image.png' : 'images/profile.jpg';
+    img.src = isHover ? 'images/image.png' : 'images/profile.png';
 }
