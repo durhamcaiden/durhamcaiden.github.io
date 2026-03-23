3a39b85627c86cb70cd125e2b0f33faa17d37602
durhamcaiden <caiden.durham@siu.edu>
2026-03-22 23:42:04 -0500

    Update rollover.js

--- js/rollover.js
+++ js/rollover.js
@@ -1,5 +1,5 @@
 function swapMainGraphic(isHover) {
     var img = document.getElementById('mainGraphic');
     if (!img) return;
-    img.src = isHover ? 'images/siu.png' : 'images/profile.jpg';
+    img.src = isHover ? 'images/siu.png' : 'images/siu.png';
 }
